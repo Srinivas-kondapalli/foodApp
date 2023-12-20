061f31acf0f1f2cf0a4876bf39739f2b6c6d7062
srinivas <srinivaskondapalli801@gmail.com>
2023-12-20 18:22:41 +0530

    contact changes

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
+import Helmet from '../assets/components/Helmet/Helmet'
+import CommonSection from '../assets/components/UI/category/common-section/CommonSection'
+import { Col, Container, Row } from 'reactstrap'
 
 function Contact() {
   return (
-    <div>
-      Contact
-    </div>
+    <Helmet title='Cart'>
+    <CommonSection title="Your Cart" />
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12'>
+           
+            <h1>Development Under Process ........ coming Sooon 😍😍😍😍</h1>
+          </Col>
+        </Row>
+      </Container>
+    </section>
+  </Helmet>
   )
 }
 
